372f7ca4aaf0f9e4f8b7a20ffd8977f13e4d5b54
chaeyoon <yoonchae125@naver.com>
2020-04-30 16:05:03 +0900

    4장 끝

---
+++ Practice/src/_04/Decorator.kt
@@ -0,0 +1,11 @@
+package _04
+
+//class DelegatingCollection<T> : Collection<T>{
+//	private val innerList =arrayListOf<T>()
+//	override val size:Int get() = innerList.size
+//	override fun isEmpty(): Boolean = innerList.isEmpty()
+//	override fun contains(element:T): Boolean = innerList.contains(element)
+//	override fun iterator(): Iterator<T> = innerList.iterator()
+//	override fun containsAll(elements: Collection<T>): Boolean = innerList.containsAll(elements)
+//}
+class DelegatingCollection<T>(innerList: Collection<T> = ArrayList<T>()):Collection<T> by innerList{}
---
+++ Practice/src/_04/anonymous_object.kt
@@ -0,0 +1,11 @@
+package _04
+
+import java.awt.event.MouseEvent
+import java.awt.event.MouseAdapter
+
+//window.addMouseListener(
+//	object: MouseAdapter(){
+//		override fun mouseClicked(e: MouseEvent){}
+//		override fun mouseEntered(e: MouseEvent){}
+//	}
+//)	
