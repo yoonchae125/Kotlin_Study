81bb942b6122b8f95546f0588d77553acee10e08
chaeyoon <yoonchae125@naver.com>
2020-05-02 21:49:57 +0900

    intelliJ

--- Practice/src/_02/YoonFam.kt
+++ Practice/src/_02/YoonFam.kt
@@ -17,14 +17,14 @@ abstract class Animated {
 	fun animateTwice() {} // final
 	// 비추상 함수는 기본적으로 final이지만 open 변환자로 override 허용 가능
 }
-data class Person(val name: String){
+data class Person(val name: String, val age:Int){
   object NameComparator:Comparator<Person>{
     override fun compare(p1:Person, p2:Person):Int = 
     	p1.name.compareTo(p2.name)
   }
 }
 object Payroll{
-  val allEmployees = arrayListOf<Person>(Person("채윤"))
+  val allEmployees = arrayListOf<Person>(Person("채윤",24))
   fun calculateSalary(){
     for(person in allEmployees){
       println(person)
@@ -48,4 +48,20 @@ fun main(args:Array<String>){
 	}
 	
 	println("12.345-6.A".split(".", "-"))
+	
+	val list = listOf(1, 2, 3, 4)
+	println(list.filter{ it % 2 ==0 }) // 짝수만 남는다.
+	println(list.map{ it * it })
+	val people = listOf(Person("Alice", 29), Person("Bob", 31))
+	println(people.map{ it.name })
+	println(people.map(Person::name))
+	println(people)
+	println(people.filter{ it.age > 30 }.map(Person::name))
+	println("채윤 ${people.maxBy(Person::age)?.name}")
+	println(people.get(1).age)
+	println(people.filter{it.age==people.maxBy(Person::age)!!.age})
+	
+	val numbers = mapOf(0 to "zero", 1 to "one")
+	println(numbers.mapValues{ it.value.toUpperCase() })
+	println(numbers.keys)
 }
--- practice/src/_02/aboutIn.kt
+++ practice/src/_02/aboutIn.kt
@@ -9,8 +9,8 @@ fun main(args:Array<String>){
 	
 }
 val number = 200
-val percentage=
-	if (number in 0..100)
-		number
-	else
-		throw IllegalArgumentException("value must be between 0 and 100: $number")
+//val percentage=
+//	if (number in 0..100)
+//		number
+//	else
+//		throw IllegalArgumentException("value must be between 0 and 100: $number")
